6cb6be97eb4455691306e8418452ab6500df010c
Aarush Naik <109808496+AarushCoding@users.noreply.github.com>
2026-02-22 14:36:39 +0000

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ DATA_FILE = "comps.txt"
 today = datetime.date.today().isoformat()
 
 # API URL: Specifically for the UK, fetching 100 results to ensure we see everything
-API_URL = f"https://www.worldcubeassociation.org/api/v0/competitions?country_iso2=GB&start_date={today}&per_page=100"
+API_URL = f"https://www.worldcubeassociation.org/api/v0/competitions?country_iso2=GB&upcoming=true"
 
 def send_telegram(msg):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
